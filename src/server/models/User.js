7b8f68c3ad5faba1f2dad81604e8b19c7bfe7f27
f <fatihkadirakin@gmail.com>
2025-03-12 12:00:00 +0300

    Add UI in User.js

---
+++ src/server/models/User.js
@@ -0,0 +1,14 @@
+// server/models/User.js
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['buyer', 'seller'], default: 'buyer' },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
